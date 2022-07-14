0c5e9a2b6978427e73a9a03ef80d05848ba6e391
Víctor Mardones <v.mardones.dev@gmail.com>
2022-07-14 19:13:24 -0400

    Adds more utility functions

--- src/main/java/board/BoardUtils.java
+++ src/main/java/board/BoardUtils.java
@@ -2,11 +2,13 @@ package board;
 
 import player.Alliance;
 
+import java.util.Comparator;
+
 public class BoardUtils {
 
     public static final int MIN_TILES = 0;
     public static final int MAX_TILES = 63;
-    private static final int ROW_LENGTH = 8;
+    private static final int NUMBER_OF_ROWS = 8;
 
     private BoardUtils() {
         throw new RuntimeException("You cannot instantiate me!");
@@ -16,8 +18,24 @@ public class BoardUtils {
         return coordinate >= MIN_TILES && coordinate <= MAX_TILES;
     }
 
+    public static int getRow(final int coordinate) {
+        return coordinate / NUMBER_OF_ROWS;
+    }
+
+    public static int getColumn(final int coordinate) {
+        return coordinate % NUMBER_OF_ROWS;
+    }
+
+    public static boolean sameRow(final int origin, final int destination) {
+        return getRow(origin) == getRow(destination);
+    }
+
+    public static boolean sameColumn(final int origin, final int destination) {
+        return getColumn(origin) == getColumn(destination);
+    }
+
     public static Alliance getTileColor(final int coordinate) {
-        if ((coordinate + Math.floor(coordinate / ROW_LENGTH) % 2) % 2 == 0) {
+        if ((coordinate + Math.floor(coordinate / NUMBER_OF_ROWS) % 2) % 2 == 0) {
             return Alliance.WHITE;
         }
         return Alliance.BLACK;
@@ -26,4 +44,5 @@ public class BoardUtils {
     public static boolean sameColor(final int origin, final int destination) {
         return getTileColor(origin) == getTileColor(destination);
     }
+
 }
--- src/test/java/board/BoardUtilsTest.java
+++ src/test/java/board/BoardUtilsTest.java
@@ -23,6 +23,36 @@ class BoardUtilsTest {
     }
 
     @Test
+    void getRow() {
+        assertEquals(7, BoardUtils.getRow(61));
+    }
+
+    @Test
+    void getColumn() {
+        assertEquals(0, BoardUtils.getColumn(16));
+    }
+
+    @Test
+    void sameRow() {
+        assertTrue(BoardUtils.sameRow(3, 4));
+    }
+
+    @Test
+    void differentRow() {
+        assertFalse(BoardUtils.sameRow(1, 60));
+    }
+
+    @Test
+    void sameColumn() {
+        assertTrue(BoardUtils.sameColumn(8, 16));
+    }
+
+    @Test
+    void differentColumn() {
+        assertFalse(BoardUtils.sameColumn(8, 18));
+    }
+
+    @Test
     void getWhiteTile() {
         assertEquals(Alliance.WHITE, BoardUtils.getTileColor(0));
     }
