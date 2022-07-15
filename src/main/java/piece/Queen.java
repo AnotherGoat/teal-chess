cb5c35609cd5f6daf8daf7e4f8fcff48f153855c
Víctor Mardones <v.mardones.dev@gmail.com>
2022-07-14 20:09:46 -0400

    Implement the queen piece

---
+++ src/main/java/piece/Queen.java
@@ -0,0 +1,25 @@
+package piece;
+
+import board.BoardUtils;
+import player.Alliance;
+
+public final class Queen extends SlidingPiece {
+
+    private final static int[] CANDIDATE_MOVE_VECTORS = {-9, -8, -7, -1, 1, 7, 8, 9};
+
+    public Queen(int position, Alliance alliance) {
+        super(position, alliance);
+    }
+
+    @Override
+    int[] getMoveVectors() {
+        return CANDIDATE_MOVE_VECTORS;
+    }
+
+    @Override
+    protected boolean isIllegalMove(int destination) {
+        return !BoardUtils.sameRow(position, destination) &&
+                !BoardUtils.sameColumn(position, destination) &&
+                !BoardUtils.sameColor(position, destination);
+    }
+}
