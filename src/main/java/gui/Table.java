036ce665e523ed68044ebfc44433fe58ca4067bd
Víctor Mardones <v.mardones.dev@gmail.com>
2022-07-20 04:08:53 -0400

    Allow the use of chess piece icons

--- src/main/java/gui/Table.java
+++ src/main/java/gui/Table.java
@@ -1,10 +1,15 @@
 package gui;
 
+import engine.board.Board;
 import engine.board.BoardUtils;
 import engine.player.Alliance;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,6 +21,7 @@ public class Table {
 
     private static final Color LIGHT_TILE_COLOR = Color.decode("#FFCE9E");
     private static final Color BLACK_TILE_COLOR = Color.decode("#D18B47");
+    private static final String PIECE_ICON_PATH = "";
 
     private final JFrame gameFrame;
     private final BoardPanel boardPanel;
@@ -83,6 +89,22 @@ public class Table {
             setPreferredSize(TILE_PANEL_DIMENSION);
             assignTileColor();
             validate();
+        }
+
+        private void assignTileIcon(final Board board) {
+            removeAll();
+
+            if (board.getTile(tileId).isOccupied()) {
+                try {
+                    final var image = ImageIO.read(new File("%s/%s%s.svg"
+                            .formatted(PIECE_ICON_PATH,
+                                    board.getTile(tileId).getPiece().getAlliance().toString().charAt(0),
+                                    board.getTile(tileId).getPiece().toString())));
+                    add(new JLabel(new ImageIcon(image)));
+                } catch (IOException e) {
+                    throw new IllegalStateException("Failed to load images!");
+                }
+            }
         }
 
         private void assignTileColor() {
